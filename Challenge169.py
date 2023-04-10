3624cb7120976c513257182d37e250bf9077d55d
1504168 <1997ismail.hosen@gmail.com>
2023-04-10 03:01:52 -0700

    Code has been updated to use relative file path.

--- Challenge169.py
+++ Challenge169.py
@@ -1,5 +1,5 @@
 import pandas as pd
-file_path = "/Users/mac/Downloads/Order Cities.xlsx"
+file_path = "Order Cities.xlsx"
 df = pd.read_excel(file_path, sheet_name="Sheet1")
 df = df.iloc[:,0:5]
 non_nan_count =  df.count().to_frame(name="Non NaN count").reset_index()
