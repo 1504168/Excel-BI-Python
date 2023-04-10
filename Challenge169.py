69afe7066835e4490ead40fd819b4f98489731a0
1504168 <1997ismail.hosen@gmail.com>
2023-04-10 02:53:54 -0700

    Challenge 169 has been solved.

---
+++ Challenge169.py
@@ -0,0 +1,8 @@
+import pandas as pd
+file_path = "/Users/mac/Downloads/Order Cities.xlsx"
+df = pd.read_excel(file_path, sheet_name="Sheet1")
+df = df.iloc[:,0:5]
+non_nan_count =  df.count().to_frame(name="Non NaN count").reset_index()
+correct_column_order = non_nan_count.sort_values(["Non NaN count","index"],ascending=[False,False])["index"].tolist()
+final_df = df[correct_column_order]
+print(final_df)
